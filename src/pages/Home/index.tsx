a5e399407c4e05d243b12cf8b11899ce858f1320
Diegojacober <diegoalencar.jacober@gmail.com>
2023-11-28 16:52:20 -0300

    Header

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,46 @@
-import React, { useState, useContext } from "react";
-
+import React, { useState, useEffect } from "react";
+import { View, ActivityIndicator } from 'react-native';
 import { useNavigation } from "@react-navigation/native";
-import { Div, Text} from "react-native-magnus";
+import { Div, Text } from "react-native-magnus";
+import Header from "../../components/Header";
+import useAuthStore from "../../stores/useAuthStore";
 
 export default function Home() {
   const navigation = useNavigation();
 
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const [getUser, accessToken] = useAuthStore(
+    (state) => [
+      state.getUser,
+      state.accessToken
+    ]
+  );
+
+  useEffect(() => {
+    function loadUser() {
+      setLoading(true)
+      getUser(accessToken)
+      setLoading(false)
+    }
+    loadUser()
+  },[])
+
+  if (loading) {
+    return (
+        <Div flex={1} pt={50} bg="darkblue" alignItems="center" justifyContent="center">
+            <ActivityIndicator size={70} color={"#3883BB"} />
+        </Div>
+    )
+}
+
   return (
-    <Div flex={1} pt={50}>
-        <Text fontSize={20}>Home Page</Text>
+    <Div flex={1} pt={50} bg="darkblue" alignItems="center">
+      <Div w={'95%'}>
+        <Header />
+        <Text color="white" fontSize={20}>Home Page</Text>
+      </Div>
+
     </Div>
   );
 }
--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,14 +1,28 @@
 import React, { useState, useContext } from "react";
 
 import { useNavigation } from "@react-navigation/native";
-import { Div, Text} from "react-native-magnus";
+import { Button, Div, Icon, Text } from "react-native-magnus";
+import useAuthStore from "../../stores/useAuthStore";
 
 export default function Profile() {
   const navigation = useNavigation();
 
+  const [logout] = useAuthStore(
+    (state) => [
+      state.logout,
+    ]
+  );
+
+  const handleLogout = () => {
+    logout()
+  }
+
   return (
     <Div flex={1} pt={50}>
-        <Text>Profile Page</Text>
+      <Text>Profile Page</Text>
+      <Button bg="red" h={50} w={50} rounded="circle" onPress={handleLogout}>
+        <Icon name="logout" color="white" fontSize={18}/>
+      </Button>
     </Div>
   );
 }
