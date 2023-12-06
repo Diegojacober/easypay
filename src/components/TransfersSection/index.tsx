a9da733731968546241e1424557b443ab72c0002
Diego Alencar Jacober <57761933898>
2023-12-06 16:56:54 -0300

    Image picker

--- src/components/TransfersSection/index.tsx
+++ src/components/TransfersSection/index.tsx
@@ -1,42 +1,49 @@
-import React from 'react'
-import { Div, Text } from 'react-native-magnus'
-import { Transfer } from '../queries/transfers/types'
-import useAuthStore from '../../stores/useAuthStore'
-import TransferCard from '../TransferCard'
+import React from "react";
+import { Div, Text } from "react-native-magnus";
+import { Transfer } from "../queries/transfers/types";
+import useAuthStore from "../../stores/useAuthStore";
+import TransferCard from "../TransferCard";
+import { FlatList } from "react-native";
 
 interface ITransfersSectionProps {
-    transfers: Transfer[]
+  transfers: Transfer[];
 }
 
 function TransfersSection({ transfers }: ITransfersSectionProps) {
-
-    const [account] = useAuthStore(
-        (state) => [
-            state.account,
-        ]
-    );
+  const [account] = useAuthStore((state) => [state.account]);
 
   return (
     <Div mt={25}>
-        <Text color='white' fontSize={16} fontWeight='700' mb={4}>Recent activity</Text>
+      <Text color="white" fontSize={16} fontWeight="700" mb={4}>
+        Recent activity
+      </Text>
 
-        {(transfers?.length > 0) ? (
-                <>
-                    {transfers.map(transferencia => (
-                        <TransferCard key={transferencia.id} transfer={{
-                            id: transferencia.id,
-                            type: account?.id !== transferencia.to_account_id ? 'sender' : 'receiver',
-                            value: transferencia.value,
-                            date: transferencia.created_at,
-                            name: ` ${transferencia.to_account.agencia}  ${transferencia.to_account.numero}`
-                        }}/>
-                    ))}
-                </>
-            ) : (
-                <Text color='gray500' fontSize={12} ml={15} mt={5}>Nenhuma transferencia...</Text>
-            )}
+      {transfers?.length > 0 ? (
+        <FlatList
+          showsVerticalScrollIndicator={false}
+          keyExtractor={(item) => item.id.toString()}
+          data={transfers}
+          renderItem={({ item }) => (
+            <TransferCard
+              key={item.id}
+              transfer={{
+                id: item.id,
+                type:
+                  account?.id !== item.to_account_id ? "sender" : "receiver",
+                value: item.value,
+                date: item.created_at,
+                name: ` ${item.to_account.agencia}  ${item.to_account.numero}`,
+              }}
+            />
+          )}
+        />
+      ) : (
+        <Text color="gray500" fontSize={12} ml={15} mt={5}>
+          Nenhuma transferencia...
+        </Text>
+      )}
     </Div>
-  )
+  );
 }
 
-export default TransfersSection
+export default TransfersSection;
--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,28 +1,234 @@
 import React, { useState, useContext } from "react";
 
 import { useNavigation } from "@react-navigation/native";
-import { Button, Div, Icon, Text } from "react-native-magnus";
+import {
+  Button,
+  Div,
+  Icon,
+  Image,
+  Input,
+  Modal,
+  Text,
+} from "react-native-magnus";
 import useAuthStore from "../../stores/useAuthStore";
-
+import {
+  Keyboard,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+} from "react-native";
+import * as ImagePicker from "expo-image-picker";
 export default function Profile() {
   const navigation = useNavigation();
 
-  const [logout] = useAuthStore(
-    (state) => [
-      state.logout,
-    ]
+  const [logout, user, account] = useAuthStore((state) => [
+    state.logout,
+    state.user,
+    state.account,
+  ]);
+  const [visible, setVisible] = useState<boolean>(false);
+
+  const openModal = () => {
+    setVisible(true);
+  };
+
+  const [avatarUrl, setAvatarUrl] = useState<string | null>(
+    user && user?.url_image
   );
 
-  const handleLogout = () => {
-    logout()
-  }
+  const gallery = async () => {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [1, 1],
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setAvatarUrl(result.assets[0].uri);
+      console.log(result.assets[0].uri);
+    }
+  };
 
   return (
-    <Div flex={1} pt={50}>
-      <Text>Profile Page</Text>
-      <Button bg="red" h={50} w={50} rounded="circle" onPress={handleLogout}>
-        <Icon name="logout" color="white" fontSize={18}/>
-      </Button>
-    </Div>
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <Div flex={1} pt={50} bg="darkblue" alignItems="center">
+        <Div w={"95%"} alignItems="center" pt={30}>
+          <TouchableOpacity
+            style={{
+              width: 90,
+              height: 90,
+              borderRadius: 50,
+              position: "absolute",
+              top: 28,
+              zIndex: 1,
+            }}
+            onPress={gallery}
+          ></TouchableOpacity>
+          <Image
+            w={85}
+            h={85}
+            rounded="circle"
+            source={{
+              uri:
+                avatarUrl === null
+                  ? `https://ui-avatars.com/api/?background=random&name=${user?.first_name} ${user?.last_name}`
+                  : avatarUrl,
+            }}
+          />
+        </Div>
+
+        <Text
+          color="white"
+          fontWeight="600"
+          fontSize={16}
+          mt={10}
+          textTransform="uppercase"
+        >
+          {user?.first_name} {user?.last_name}
+        </Text>
+
+        <TouchableOpacity
+          style={{
+            marginTop: 15,
+            backgroundColor: "#3883BB",
+            borderRadius: 4,
+            paddingHorizontal: 20,
+            paddingVertical: 12,
+          }}
+          onPress={openModal}
+        >
+          <Text
+            fontSize={16}
+            fontWeight="500"
+            color="white"
+            w={"70%"}
+            textAlign="center"
+          >
+            See account details
+          </Text>
+        </TouchableOpacity>
+
+        <Modal isVisible={visible} h={"50%"}>
+          <Div mt={20} px={10}>
+            <Button
+              bg="gray400"
+              h={48}
+              w={48}
+              mt={-10}
+              position="absolute"
+              right={15}
+              rounded="circle"
+              onPress={() => {
+                setVisible(false);
+              }}
+            >
+              <Icon color="gray800" name="closecircle" fontSize={18} />
+            </Button>
+            <Text fontSize={20} w={"80%"} color="lightblue" fontWeight="300">
+              Account Details
+            </Text>
+
+            <Div mt={10}>
+              <Div>
+                <Text color="#7e7e7e" fontSize={12}>
+                  Institute:
+                </Text>
+                <Input
+                  borderBottomColor="white"
+                  borderTopColor="#000"
+                  keyboardType="numeric"
+                  fontWeight="800"
+                  h={50}
+                  mb={5}
+                  editable={false}
+                  selectTextOnFocus={false}
+                  value="EASYPAY BANK LTDA."
+                  bg="#CCC"
+                />
+              </Div>
+
+              <Div>
+                <Div flexDir="row" w={"100%"} style={{ gap: 10 }}>
+                  <Div mt={5} flex={1}>
+                    <Text color="#7e7e7e" fontSize={12} mb={5}>
+                      AGENCY:
+                    </Text>
+                    <Input
+                      borderBottomColor="white"
+                      borderTopColor="#000"
+                      keyboardType="numeric"
+                      h={50}
+                      fontWeight="500"
+                      fontSize={15}
+                      mb={20}
+                      editable={false}
+                      selectTextOnFocus={false}
+                      value="0001"
+                      textAlign="center"
+                      bg="#CCC"
+                    />
+                  </Div>
+
+                  <Div mt={5} flex={2}>
+                    <Text color="#7e7e7e" fontSize={12} mb={5}>
+                      NUMBER:
+                    </Text>
+                    <Input
+                      borderBottomColor="white"
+                      borderTopColor="#000"
+                      keyboardType="numeric"
+                      h={50}
+                      fontWeight="500"
+                      fontSize={13}
+                      mb={20}
+                      maxLength={8}
+                      editable={false}
+                      selectTextOnFocus={false}
+                      value={account?.numero}
+                      bg="#CCC"
+                    />
+                  </Div>
+                </Div>
+              </Div>
+
+              <Div>
+                <Text color="#7e7e7e" fontSize={12}>
+                  NAME:
+                </Text>
+                <Input
+                  borderBottomColor="white"
+                  borderTopColor="#000"
+                  fontWeight="800"
+                  h={50}
+                  mb={5}
+                  editable={false}
+                  selectTextOnFocus={false}
+                  value={`${user?.first_name} ${user?.last_name}`}
+                  bg="#CCC"
+                />
+              </Div>
+
+              <Div>
+                <Text color="#7e7e7e" fontSize={12}>
+                  CPF:
+                </Text>
+                <Input
+                  borderBottomColor="white"
+                  borderTopColor="#000"
+                  keyboardType="numeric"
+                  fontWeight="800"
+                  h={50}
+                  mb={5}
+                  editable={false}
+                  selectTextOnFocus={false}
+                  value={user?.cpf}
+                  bg="#CCC"
+                />
+              </Div>
+            </Div>
+          </Div>
+        </Modal>
+      </Div>
+    </TouchableWithoutFeedback>
   );
 }
